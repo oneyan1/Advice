7fda77d0859334397d916c8b692a64803ef6ca63
oneyan1 <yangrodno@gmail.com>
2019-09-15 23:57:21 +0200

    responcive in work, xl-large screen

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,9 @@ window.addEventListener("DOMContentLoaded", ()=>{
     let btn = document.querySelectorAll(".button-modal");
     let popup = document.querySelector(".popup");
     let modalClose = popup.querySelector("#close");
+    let certificateImgSmall = document.querySelector("#certificate-small");
+    let certificateImgFull = document.querySelector("#certificate-full");
+    let overlay = document.querySelector(".overlay");
 
     btn.forEach((item)=>{
         item.addEventListener("click", ()=>{
@@ -14,4 +17,37 @@ window.addEventListener("DOMContentLoaded", ()=>{
     modalClose.addEventListener("click", ()=>{
         popup.classList.remove("popup-active");
     });
+
+    certificateImgSmall.addEventListener("click", ()=>{
+        overlay.style.display = "flex";
+    });
+
+    overlay.addEventListener("click", (event)=>{
+        if(event.target === overlay.querySelector("img")){
+            overlay.querySelector("img").style.height = "100%";
+        }
+        else{
+            overlay.style.display = "none";
+        } 
+    });
+
+    let slider = document.querySelector(".reviews-slider");
+    let sliderNonActive = document.querySelectorAll(".reviews-slider-nonactive");
+    let prevBtn = slider.querySelector(".left-arrow");
+    let nextBtn = slider.querySelector(".right-arrow");
+
+    nextBtn.addEventListener("click", ()=>{
+        
+        sliderNonActive.forEach((item)=>{
+            slider.classList.remove("reviews-slider");
+            slider.classList.add("reviews-slider-nonactive");
+            item.classList.remove("reviews-slider-nonactive");
+            item.classList.add("reviews-slider");
+        })
+        // slider.classList.remove("reviews-slider");
+        // slider.classList.add("reviews-slider-nonactive");
+        // sliderNonActive[0].classList.remove("reviews-slider-nonactive");
+        // sliderNonActive[0].classList.add("reviews-slider");
+    });
 });
+
